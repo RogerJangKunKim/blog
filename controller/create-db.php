31697ae561e0d479d82427604c1b0c8b0782a24d
Roger JangKun Kim <rb2stk@gmail.com>
2014-10-28 11:10:46 -0700

    updated for video 13

--- controller/create-db.php
+++ controller/create-db.php
@@ -15,13 +15,17 @@
 	$exists = $connection->select_db($database);
 	
 
-	//checking if database doesn't exists. queries are questions/commands sent to the database.
+	//checking if database doesn't exists. queries are questions/commands sent to the database. ONLY runs if database does not exist.
 	if(!$exists){
 		$query = $connection->query("CREATE DATABASE $database"); /*will say true if successful, false if not*/
 
 		if($query){ /*check if successful/not*/
 			echo "Successfully created database " . $database;
 		}
+	}
+	//if there is a database, this else statement will run.
+	else{
+		echo "Database already exists.";
 	}
 	//close the connection
 	$connection->close();
