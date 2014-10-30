fbd39e26e74f50c168da396a7143011d7871be92
Roger JangKun Kim <rb2stk@gmail.com>
2014-10-30 11:25:32 -0700

    video 17 10/30

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta>
-	<title></title>
-</head>
-<body>
-	<?php
+
+<?php
+	//separated for easier maintenance.
+	//header can be styled with style sheets.
+	require_once(__DIR__ . "/view/header.php");
+	//links to navigation.php and will show a link to post.php
+	require_once(__DIR__ . "/view/navigation.php");
 	//include info from create-db.php
 	require_once(__DIR__ . "/controller/create-db.php");
-	?>
-</body>
-</html>
+	require_once(__DIR__ . "/view/footer.php");
+?>
