689ae7e8fedc6bae4b5fd6b62a784b7d291e57fb
Roger JangKun Kim <rb2stk@gmail.com>
2014-11-03 12:03:50 -0800

    vid 18 11/3

--- view/navigation.php
+++ view/navigation.php
@@ -1,5 +1,9 @@
+<?php //corrected path to post.php removed the error of object not found when clicking the href link.
+//only need contents in config in order to change an image.
+	require_once(__DIR__ . "/../model/config.php");
+?>
 <nav>
 	<ul>
-		<li><a href="../post.php">Blog Post Form</a></li>
+		<li><a href="<?php echo $path . "post.php" ?>">Blog Post Form</a></li>
 	</ul>
 </nav>
