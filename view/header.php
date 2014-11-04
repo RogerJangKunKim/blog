b21954db51544e92c1accaabf0cb88ad42836806
Roger JangKun Kim <rb2stk@gmail.com>
2014-11-04 11:25:28 -0800

    comments on video 17

--- view/header.php
+++ view/header.php
@@ -3,5 +3,8 @@
 <head>
 <meta>
 	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/main1.css">
 </head>
 <body>
--- view/navigation.php
+++ view/navigation.php
@@ -2,8 +2,14 @@
 //only need contents in config in order to change an image.
 	require_once(__DIR__ . "/../model/config.php");
 ?>
+
 <nav>
 	<ul>
-		<li><a href="<?php echo $path . "post.php" ?>">Blog Post Form</a></li>
+		<li> <!--$path is created in config. path can be accessed from the config.php file by the require_once at the top of this file 
+		$path links all project files together for easier access.
+		php code echos the $path and the string.
+		-->
+			<a href="<?php echo $path . "post.php" ?> ">Blog Post Form</a>
+		</li>
 	</ul>
-</nav>
+</nav>	
