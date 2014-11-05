c1aa464e0fa8a9298c68ce033b4ff0dd4f2f37ee
Roger JangKun Kim <rb2stk@gmail.com>
2014-11-05 11:13:19 -0800

    vid 19 11-5-2014

---
+++ view/form.php
@@ -0,0 +1,12 @@
+<h1>Create Blog Post</h1>
+
+<form>
+	<!-- try to use the terms you used from your table. -->
+		<label for="title">Title: </label>
+		<input type="text" name="title"/>
+	</div>
+	<!-- text area lets you inpur more text than label.  -->
+		<label for="post">Post: </label>
+		<textarea name="post"></textarea>
+	</div>
+</form>
--- view/header.php
+++ view/header.php
@@ -4,7 +4,7 @@
 <meta>
 	<title></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="../css/main1.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main1.css">
 </head>
 <body>
