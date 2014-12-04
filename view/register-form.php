d1964dd34585424f2c0203d8fb778bcb440437e3
Roger JangKun Kim <rb2stk@gmail.com>
2014-12-04 11:16:47 -0800

    video 39. created the register user form.

--- view/register-form.php
+++ view/register-form.php
@@ -1,18 +1,29 @@
+<?php 
+// we now have access to the path variable. correct path to reach certain files/folders without difficulty.
+	require_once(__DIR__ . "/../model/config.php");
+
+	$date = new DateTime('today');
+?>
+
 <!-- allow users to register -->
-<form>
+<h1>Register</h1>
+<form method="post" action="<?php echo $path . "controller/create-user.php"; ?>">
 	<div>
 		<label for="email">Email: </label>
 		<input type="text" name="email" />
 	</div>
+
 	<div>
 		<label for="username">Username: </label>
 		<input type="text" name="username" />
 	</div>
+
 	<div>
 		<label for="password">Password: </label>
 		<!-- input type is password because we don't want the user to see what they are typing, so it will appear as bullet points -->
 		<input type="password" name="password" />
 	</div>
+
 	<div>
 		<button type="submit">Submit</button>
 	</div>
