6408c322577203d1f1ae1452a02fe0cfd1ec7e6f
Roger JangKun Kim <rb2stk@gmail.com>
2014-12-10 11:29:19 -0800

    shows posts on index vid 45

--- index.php
+++ index.php
@@ -8,4 +8,5 @@
 	//include info from create-db.php
 	require_once(__DIR__ . "/controller/create-db.php");
 	require_once(__DIR__ . "/view/footer.php");
+	require_once(__DIR__ . "/controller/read-post.php");
 ?>
