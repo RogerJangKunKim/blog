7fe48bfa0b61e49db0c12d6148e5cb3bd642ef74
Roger JangKun Kim <rb2stk@gmail.com>
2014-12-12 11:29:44 -0800

    video 48. tells user is password/username is wrong.

--- controller/login-user.php
+++ controller/login-user.php
@@ -5,3 +5,20 @@ $username = filter_input (INPUT_POST, "username", FILTER_SANITIZE_STRING);
 $password = filter_input (INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
 $query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username = '$username'");
+
+//num_rows how many rows we will retrieve from database.
+if($query->num_rows==1){
+	$rows = $query->fetch_array();
+	// case sensitive.
+	if($row["password"] === crypt($password, $row["salt"])){
+		echo "<p>Login Successful</p>";
+	}
+	else{
+		echo "<p>Invalid Username and Password</p>";
+	}
+}
+else{
+	echo "<p>Invalid Username and Password</p>";
+}
+
+//checks if hashed password=new hashed password
