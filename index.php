484766760c6e182a2e507f6af4901778dba8116e
Roger JangKun Kim <rb2stk@gmail.com>
2014-12-15 12:16:09 -0800

    vid 50

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 
 <?php
+require_once(__DIR__ . "/controller/login-verify.php");
 //separated for easier maintenance.
 //header can be styled with style sheets.
 require_once(__DIR__ . "/view/header.php");
-//links to navigation.php and will show a link to post.php
-require_once(__DIR__ . "/view/navigation.php");
+if(authenticateUser())  {
+	//links to navigation.php and will show a link to post.php
+	//only will run if user has been authenticated.
+	require_once(__DIR__ . "/view/navigation.php");
+}
 //include info from create-db.php
 require_once(__DIR__ . "/controller/create-db.php");
 require_once(__DIR__ . "/view/footer.php");
